9621d4713961206f83bec967fb972d0e0354a4a5
Jean Michael Connory <michaelrhcp@gmail.com>
2025-02-22 13:52:39 -0300

    Update firebaseConnection.ts

--- src/services/firebaseConnection.ts
+++ src/services/firebaseConnection.ts
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDcLxix75NQlLiKeGH8lbdF1qFFrnhqWuo",
-  authDomain: "tarefasplus-2ed66.firebaseapp.com",
-  projectId: "tarefasplus-2ed66",
-  storageBucket: "tarefasplus-2ed66.firebasestorage.app",
-  messagingSenderId: "1024022542846",
-  appId: "1:1024022542846:web:155586c0d67ca84e7f68af"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 // Initialize Firebase
